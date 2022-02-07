59e9f4cca9659ee0e258d6f8782e941fdb66eee3
Tonje <91598255+tonjetj@users.noreply.github.com>
2022-02-07 14:40:01 +0100

    added question info

---
+++ js/question-1.js
@@ -0,0 +1,5 @@
+// convert the function below to an arrow function
+
+function getRemainder(a, b) {
+  return a % b;
+}
---
+++ js/question-2.js
@@ -0,0 +1,8 @@
+// Make a call to the Rawg API
+
+// loop through the results and display the following properties in HTML, but only for the first 8 results:
+// name
+// rating
+// number of tags (not the tag details, just the amount of tags)
+
+// you can use either regular promise or async/await syntax to make the call
