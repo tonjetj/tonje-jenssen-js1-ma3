0def1b205574e64449f053cdf97bf805ed3b006b
Tonje <91598255+tonjetj@users.noreply.github.com>
2022-02-11 17:04:41 +0100

    update
    added try for loop and innerHTML div message inside try-catch blocks.
    added styling on css.

--- js/question-2.js
+++ js/question-2.js
@@ -12,9 +12,26 @@ async function getResults() {
   const apiUrl =
     "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=d84791b15e7947698d328295cb4e3946";
   try {
-    const response = await fetch(apiUrl);
-    const results = await response.json();
-    console.log(results);
+    const resP = await fetch(apiUrl);
+    const resU = await resP.json();
+    console.log(resU.results);
+
+    const facts = resU.results;
+
+    resultsContainer.innerHTML = "";
+
+    for (let i = 0; i < facts.length; i++) {
+      console.log(facts[i].name);
+      console.log(facts[i].rating);
+      console.log(facts[i].tags);
+      if (i === 8) {
+        break;
+      }
+      resultsContainer.innerHTML += `<div class="result">
+      Game: ${facts[i].name}
+      Rating: ${facts[i].rating}
+      Tags: ${facts[i].tags}</div>`;
+    }
   } catch (error) {
     console.log(error);
     resultsContainer.innerHTML = err("error", error);
